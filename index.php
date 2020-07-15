8b4fe0a6139a054b0789cce6920badae45a06a4c
Thi Huyen Nguyen <63579575+Huyen059@users.noreply.github.com>
2020-07-15 22:18:57 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -105,6 +105,22 @@ if (!empty($_GET['searchPoke'])) {
             <h1>Pokemon</h1>
         </div>
     </header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Pokemon</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="category.php">Category</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
     <main class="container">
         <div class="pokedexCtn">
             <img src="img/pokedexpaint.png" alt="pokedex">
